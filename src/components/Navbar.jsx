1ebaf77cb392cdec7ea8447ea709561d6feaa69a
towhidul1999 <towhidulsislam@gmail.com>
2024-09-04 18:09:03 +0600

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,14 @@ const Navbar = () => {
             <div>
                 <Link to="/"><h1 className='text-white ml-80 mt-6 text-3xl font-extrabold cursor-pointer'>Towhidul Islam</h1></Link>
             </div>
-            <div className='ml-40 mt-8 space-x-10 text-white font-extrabold flex'>
-
+            <div id='button' className='ml-40 mt-8 space-x-10 text-white font-extrabold flex sm:hidden md:flex lg:flex xl:flex 2xl:flex'>
                 <Link to="/"><button className='hover:border-b-4 hover:border-custom-blue pb-1'>Home</button></Link>
                 <Link to="/about"><button className='hover:border-b-4 hover:border-custom-blue pb-1'>About</button></Link>
                 <Link to="/project"><button className='hover:border-b-4 hover:border-custom-blue pb-1'>Project</button></Link>
                 <Link to="/article"><button className='hover:border-b-4 hover:border-custom-blue pb-1'>Article</button></Link>
                 <Link to="/resume"><button className='hover:border-b-4 hover:border-custom-blue pb-1'>Resume</button></Link>
-
-
             </div>
+
             <div className='ml-40 mt-6 bg-custom-blue h-8 w-40 rounded-md hover:bg-gray-600 transition ease-in-out delay-150 flex items-center justify-center'>
                 <button className='text-white'>
                     git
@@ -27,19 +25,29 @@ const Navbar = () => {
             {/* Responsive button design */}
             <div>
 
-                <button class="bg-white p-3 rounded-md block">
-                    <div class="grid grid-cols-3 gap-1 w-6 h-6">
-                        <div class="bg-gray-500 w-2 h-2 rounded-sm"></div>
-                        <div class="bg-gray-500 w-2 h-2 rounded-sm"></div>
-                        <div class="bg-gray-500 w-2 h-2 rounded-sm"></div>
-                        <div class="bg-gray-500 w-2 h-2 rounded-sm"></div>
-                        <div class="bg-gray-500 w-2 h-2 rounded-sm"></div>
-                        <div class="bg-gray-500 w-2 h-2 rounded-sm"></div>
-                        <div class="bg-gray-500 w-2 h-2 rounded-sm"></div>
-                        <div class="bg-gray-500 w-2 h-2 rounded-sm"></div>
-                        <div class="bg-gray-500 w-2 h-2 rounded-sm"></div>
-                    </div>
+                <button
+                    className="p-3 rounded-md hidden sm:block md:hidden"
+                    onClick={() => {
+                        console.log('meow')
+                        const buttonDiv = document.getElementById('button');
+                        buttonDiv.classList.toggle('hidden');
+                    }}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
                 </button>
+
+
+
             </div>
 
 
