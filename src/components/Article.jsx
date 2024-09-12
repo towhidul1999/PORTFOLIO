8b62ac33f738fb5ea643a1f70226c22fd243b346
towhidul1999 <towhidulsislam@gmail.com>
2024-09-12 16:00:25 +0600

    article and project code optimize

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,14 +4,40 @@ const Article = () => {
     return (
         <>
             <div className="bg-custom-black h-screen flex justify-center items-center space-x-8 p-10">
-                <div className="bg-custom-blue h-64 w-64 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:rotate-2 flex justify-center items-center text-white text-2xl font-semibold">
-                    Meow Article
+                <div className="bg-custom-blue h-64 w-64 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:rotate-2 justify-center       items-center text-white text-2xl font-semibold">
+
+                    <div className='rounded-2xl bg-slate-400 object-cover'>
+                        <img src="../../video/article1.png" alt="Project" />
+                    </div>
+
+                    <div className=''>
+                        Lorem, ipsum dolor sit amet consectetur.
+                    </div>
+
                 </div>
-                <div className="bg-custom-blue h-64 w-64 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:-rotate-2 flex justify-center items-center text-white text-2xl font-semibold">
-                    Meow Article
+
+                <div className="bg-custom-blue h-64 w-64 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:-rotate-2 justify-center items-center text-white text-2xl font-semibold">
+
+                    <div className='rounded-2xl bg-slate-400 object-cover'>
+                        <img src="../../video/article1.png" alt="Project" />
+                    </div>
+
+                    <div className=''>
+                        Lorem, ipsum dolor sit amet consectetur.
+                    </div>
+
                 </div>
-                <div className="bg-custom-blue h-64 w-64 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:rotate-2 flex justify-center items-center text-white text-2xl font-semibold">
-                    Meow Article
+
+                <div className="bg-custom-blue h-64 w-64 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:rotate-2 justify-center items-center text-white text-2xl font-semibold">
+
+                    <div className='rounded-2xl bg-slate-400 object-cover'>
+                        <img src="../../video/article1.png" alt="Project" />
+                    </div>
+
+                    <div className=''>
+                        Lorem, ipsum dolor sit amet consectetur.
+                    </div>
+
                 </div>
             </div>
 
